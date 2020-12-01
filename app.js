9e5957b173cec55c5022b0d2460379386dea5748
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-12-01 01:56:59 +0100

    Adds second test route also sets up nodemon

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response)=> {
-    response.send('I love Treehouse!');
+app.get('/', (req, res)=> {
+    res.send('<h1>I love Pat!</h1>');
 });
 
-app.listen(3000);
+app.get('/hello', (req, res)=> {
+    res.send('<h1>Hello!</h1>');
+});
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000');
+});
