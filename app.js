811b345ccc57260df0577874e9484787b54a5d27
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-12-02 23:05:35 +0100

    Adds cookie method

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
 app.set('view engine', 'pug');
 
 app.get('/', (req, res)=> {
@@ -10,7 +12,7 @@ app.get('/', (req, res)=> {
 });
 
 app.get('/hello', (req, res)=> {
-    res.render('hello');
+    res.render('hello', {name: req.cookies.username });
 });
 
 app.post('/hello', (req, res)=> {
