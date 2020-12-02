cf2539480a02c5d0c3bc7d4a8bfb5fa2d49b4df6
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-12-03 00:08:25 +0100

    Adds Goodbye button to clear cookies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -8,22 +9,36 @@ app.use(cookieParser());
 app.set('view engine', 'pug');
 
 app.get('/', (req, res)=> {
-    const name =  req.cookies.username;
-    res.render('index', {name});
-});
-
-app.get('/hello', (req, res)=> {
-    res.render('hello');
-});
-
-app.post('/hello', (req, res)=> {
-    console.dir(req.body);
-    res.cookie('username', req.body.username)
-    res.redirect('/');
+    const name = req.cookies.username;
+    if (name) {
+        res.render('index', {name});
+    } else {
+        res.redirect('/hello');
+    }
 });
 
 app.get('/cards', (req, res)=> {
-    res.render('card', {prompt: "Who is buried in Grant's tomb?", colors  });
+    res.render('card', {prompt: "Who is buried in Grant's tomb?"});
+});
+
+app.get('/hello', (req, res)=> {
+    const name =  req.cookies.username;
+    if (name) {
+        res.redirect('/');
+    } else {
+        res.render('hello');
+    }
+});
+
+app.post('/hello', (req, res)=> {
+    //console.dir(req.body);
+    res.cookie('username', req.body.username);
+    res.redirect('/');
+});
+
+app.post('/goodbye', (req, res)=> {
+    res.clearCookie('username');
+    res.redirect('/hello');
 });
 
 app.listen(3000, () => {
