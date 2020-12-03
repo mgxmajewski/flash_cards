e952c87477abe5dbdc9b92f8426c20104b9f4b75
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-12-03 23:41:52 +0100

    Adds router

--- app.js
+++ app.js
@@ -5,59 +5,38 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
+
 app.use(cookieParser());
 app.set('view engine', 'pug');
 
-app.use((req,res, next) =>{
-    console.log("Hello");
-    const err = new Error('Oh noes!');
-    err.status = 500;
-    next(err);
-});
+const routes = require('./routes');
+
+app.use(routes);
+
+
+// app.use((req,res, next) =>{
+//     console.log("Hello");
+//     const err = new Error('Oh noes!');
+//     err.status = 500;
+//     next(err);
+// });
+
 
 app.use((req,res, next) =>{
     console.log(req.message);
     next();
 });
 
-
-app.get('/', (req, res)=> {
-    const name = req.cookies.username;
-    if (name) {
-        res.render('index', {name});
-    } else {
-        res.redirect('/hello');
-    }
+app.use((req, res, next) => {
+   const err = new Error('Not found');
+   err.status = 404;
+   next(err);
 });
 
-app.get('/cards', (req, res)=> {
-    res.render('card', {prompt: "Who is buried in Grant's tomb?"});
-});
-
-app.get('/hello', (req, res)=> {
-    const name =  req.cookies.username;
-    if (name) {
-        res.redirect('/');
-    } else {
-        res.render('hello');
-    }
-});
-
-app.post('/hello', (req, res)=> {
-    //console.dir(req.body);
-    res.cookie('username', req.body.username);
-    res.redirect('/');
-});
-
-app.post('/goodbye', (req, res)=> {
-    res.clearCookie('username');
-    res.redirect('/hello');
-});
-
-app.use((err, rew, res, next)=> {
+app.use((err, req, res, next)=> {
     res.locals.error = err;
     res.status(err.status);
-    res.render('error', err);
+    res.render('error');
 });
 
 
