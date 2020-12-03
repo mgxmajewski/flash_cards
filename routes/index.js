35f520e108c8e3c1d196ea7e6d4d9e4ff2c31c79
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-12-03 23:46:36 +0100

    Adds cords rout in router

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,7 @@ router.get('/', (req, res)=> {
     }
 });
 
-router.get('/cards', (req, res)=> {
-    res.render('card', {prompt: "Who is buried in Grant's tomb?"});
-});
+
 
 router.get('/hello', (req, res)=> {
     const name =  req.cookies.username;
